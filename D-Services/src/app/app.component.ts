70e4b84362131e4187ceb23ffa69171d07fbd1d7
Wajdzik Maciej <wajdzik.maciej@gmail.com>
2017-12-28 22:45:30 +0100

    Services. Add logging and accounts service.

--- D-Services/src/app/app.component.ts
+++ D-Services/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountsService} from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  accounts: string[];
+
+  constructor(private accountsService: AccountsService) {
+  }
+
+  ngOnInit(): void {
+    this.accounts = this.accountsService.accounts;
+  }
 }
--- D-Services/src/app/app.module.ts
+++ D-Services/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {NewAccountComponent} from './account/new-account/new-account.component';
+import {AccountItemComponent} from './account/account-item/account-item.component';
+import {AccountsService} from './accounts.service';
+import {LoggingService} from './logging.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewAccountComponent,
+    AccountItemComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
