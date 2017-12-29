df905ffee9f218ed1dcfdc1b9a4b05f40ac4e99e
Wajdzik Maciej <wajdzik.maciej@gmail.com>
2017-12-29 14:08:31 +0100

    Recipes. Add services.

---
+++ Z-Recipes/src/app/recipes/recipe.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Recipe} from './recipe.model';
+
+@Injectable()
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe('Recipe #1', 'This is a simple description', 'https://www.bbcgoodfood.com/sites/default/files/' +
+      'recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg', null),
+    new Recipe('Recipe #2', 'This is another description', 'https://cdn2.tmbi.com/TOH/Images/Photos/37/300x300/' +
+      'exps25157_FM153592B03_18_12b.jpg', null)
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
