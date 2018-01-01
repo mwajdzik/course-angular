d7bfdf701cd3e8e2b49fee4bce87d5c7b8a27be0
Wajdzik Maciej <wajdzik.maciej@gmail.com>
2018-01-01 12:08:06 +0100

    Routing. Not found + redirect.

--- E-Routing/src/app/app.module.ts
+++ E-Routing/src/app/app.module.ts
@@ -9,16 +9,19 @@ import {ServersComponent} from './servers/servers.component';
 import {UserComponent} from './users/user/user.component';
 import {UsersService} from './users/users.service';
 import {EditUserComponent} from './users/edit-user/edit-user.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'home', redirectTo: ''},
   {path: 'servers', component: ServersComponent},
   {
     path: 'users', component: UsersComponent, children: [
       {path: ':id', component: UserComponent},
       {path: ':id/edit', component: EditUserComponent}
     ]
-  }
+  },
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -28,7 +31,8 @@ const appRoutes: Routes = [
     UsersComponent,
     ServersComponent,
     UserComponent,
-    EditUserComponent
+    EditUserComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
