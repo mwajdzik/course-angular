7a934b15249c1e00ee257050698c77803eba2fe4
Wajdzik Maciej <wajdzik.maciej@gmail.com>
2018-01-23 16:29:47 +0100

    Recipes Redux Basic animations.

--- G-Animations/src/app/app.component.ts
+++ G-Animations/src/app/app.component.ts
@@ -1,14 +1,46 @@
 import {Component} from '@angular/core';
-import {trigger} from '@angular/animations';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(600)),
+      transition('shrunken <=> *', animate(900)),
+    ])
+  ]
 })
 export class AppComponent {
+
+  state = 'normal';
+  wildState = 'normal';
+
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item) {
     this.list.push(item);
+  }
+
+  onAnimate() {
+    this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    this.wildState = this.wildState === 'normal' ? 'highlighted' : 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 }
