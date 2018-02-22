f857ca20745cf3986f2dd666da7f758dc379373a
Wajdzik Maciej <wajdzik.maciej@gmail.com>
2018-02-22 09:39:44 +0100

    Angular - manual setup Dev config.

--- H-ManualSetup/src/app/app-routing.module.ts
+++ H-ManualSetup/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {HomeComponent} from './home.component';
 
 const appRoutes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'users', loadChildren: './users/users.module#UserModule'}
+    {path: 'users', loadChildren: './users/users.module#UsersModule'}
 ];
 
 @NgModule({
@@ -12,6 +12,7 @@ const appRoutes: Routes = [
         RouterModule.forRoot(appRoutes)
     ],
     exports: [
+        RouterModule
     ]
 })
 export class AppRoutingModule {
