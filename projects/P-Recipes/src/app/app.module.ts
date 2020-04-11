254a070ba3edcd50aa50e9d8f69ee5e527439856
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2020-04-11 15:31:04 +0200

    Angular Recipes Recipes edit form.

--- projects/P-Recipes/src/app/app.module.ts
+++ projects/P-Recipes/src/app/app.module.ts
@@ -15,6 +15,7 @@ import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [
     ShoppingListService
