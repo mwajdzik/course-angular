76a97e280d12de54dae0317205bf3e853280810b
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2020-04-11 15:08:26 +0200

    Angular Recipes Recipes edit form.

--- projects/P-Recipes/src/app/recipes/recipe.service.ts
+++ projects/P-Recipes/src/app/recipes/recipe.service.ts
@@ -2,20 +2,27 @@ import {Injectable} from "@angular/core";
 import {Recipe} from "./recipe.model";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs";
 
 @Injectable()
 export class RecipeService {
+
+  private recipesChanged = new Subject<Recipe[]>();
 
   constructor(private shoppingListService: ShoppingListService) {
   }
 
   private recipes: Recipe[] = [
-    new Recipe('Pork', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet cum illo itaque, officia quam reprehenderit totam? Accusamus, ad ea, et fugit iure maiores molestias necessitatibus sapiente sed sequi vero!',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [new Ingredient('Meet', 1), new Ingredient('French Fries', 20)]),
-    new Recipe('Chicken', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet, autem dolores eius explicabo illo iusto laboriosam libero maiores necessitatibus officia optio placeat praesentium qui recusandae reiciendis rerum veritatis voluptatibus!',
-      'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_1280,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg',
-      [new Ingredient('Meet', 1), new Ingredient('Potato', 5)])
+    new Recipe(
+      'Tasty Schnitzel', 'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Big Fat Burger', 'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    )
   ];
 
   public getRecipes() {
@@ -28,5 +35,24 @@ export class RecipeService {
 
   public addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  public getRecipesChanged() {
+    return this.recipesChanged;
+  }
+
+  public addRecipe(newRecipe: Recipe) {
+    this.recipes.push(newRecipe);
+    this.recipesChanged.next(this.getRecipes());
+  }
+
+  public editRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.getRecipes());
+  }
+
+  public deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.getRecipes());
   }
 }
