df7e05cd60b2d8ad1131a4d0fc8e6a7c24dd8e70
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2020-04-24 20:52:05 +0200

    Remove old projects

--- old-projects/H-ManualSetup/src/app/home.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    template: `<h1>{{text}}</h1>`,
-    styles: ['h1 {color: red}']
-})
-export class HomeComponent {
-    text = 'Home';
-}
